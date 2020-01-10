5af5f077463b6296ffb44ad246c059b1740d51cd
darioso <juniorosorio47@gmail.com>
2020-01-10 20:31:21 +0800

    Finished prettier configuration

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom'
 import { Container, Cart } from './styles'
 import logo from '../../assets/images/logo.svg'
 
-export default function Header () {
+export default function Header() {
   return (
     <Container>
       <Link to="/">
         <img src={logo} alt="RocketShoes" />
       </Link>
-
       <Cart to="/cart">
         <div>
-          <strong>Meu Carrinho</strong>
-          <span>3 Itens</span>
-        </div>
-      </Cart>
+          <strong> Meu Carrinho </strong> <span> 3 Itens </span>{' '}
+        </div>{' '}
+      </Cart>{' '}
     </Container>
   )
 }
--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -2,8 +2,6 @@ import React from 'react'
 
 // import { Container } from './styles'
 
-export default function Cart () {
-  return (
-    <h1>Cart</h1>
-  )
+export default function Cart() {
+  return <h1> Cart </h1>
 }
